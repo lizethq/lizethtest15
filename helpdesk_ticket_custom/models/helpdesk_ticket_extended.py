652c4d27742acbfac352844689555e5c01254f0d
jotate70 <42453302+jotate70@users.noreply.github.com>
2021-11-30 07:54:53 -0500

    Actualización

--- helpdesk_ticket_custom/models/helpdesk_ticket_extended.py
+++ helpdesk_ticket_custom/models/helpdesk_ticket_extended.py
@@ -11,15 +11,16 @@ class helpdesk_ticket_extended(models.Model):
     x_family = fields.Many2one(comodel_name='helpdesk_family', string='Familia', required="True")
     x_sub_group = fields.Many2one(comodel_name='helpdesk_sub_group', string='Sub grupo', required="True")
 
+    # Se aplica un decorador que detecta el cambio del campo partner_id
+    @api.onchange('partner_id')
+    def _domain_ochange_x_partner(self):
+        return {'domain': {'x_project': [('partner_id', "=", self.partner_id.id)]}}
+
     # Se aplica un decorador que detecta el cambio del campo x_familia
     @api.onchange('x_family')
     def _domain_ochange_x_familia(self):
         return{'domain': {'x_sub_group': [('x_family', "=", self.x_family.id)]}}
 
-    # Se aplica un decorador que detecta el cambio del campo partner_id
-    @api.onchange('partner_id')
-    def _domain_ochange_x_familia(self):
-        return {'domain': {'x_project': [('partner_id', "=", self.partner_id.id)]}}
 
 
 
--- helpdesk_ticket_custom/models/helpdesk_user_extended.py
+++ helpdesk_ticket_custom/models/helpdesk_user_extended.py
@@ -2,8 +2,8 @@
 from odoo import models, fields
 
 # heredamos del modelo usuarios
-class helpdesk_partner_extended(models.Model):
-    _inherit = 'res.partner'
+class helpdesk_user_extended(models.Model):
+    _inherit = 'res.users'
 
     x_project = fields.Many2many(comodel_name='helpdesk_project',
                                  relation='x_helpdesk_project_res_partner_rel',
