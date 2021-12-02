518f8b50d4845f5897a0e01c83fc3c60085b48a8
jotate70 <42453302+jotate70@users.noreply.github.com>
2021-12-02 18:38:11 -0500

    Delete helpdesk_ticket_custom directory

--- helpdesk_ticket_custom/models/helpdesk_users_extended.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions
-
-# heredamos del modelo usuarios
-class helpdesk_users_extended(models.Model):
-    _inherit = 'res.users'
-
-    x_project = fields.Many2many(comodel_name='helpdesk_project', relation='helpdesk_project_relation', columnn1='id', columnn2='name', string='Proyecto')
